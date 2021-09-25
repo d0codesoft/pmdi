9319eb3be1c6548f2dc6c8f69d0935272671db1c
RedPr0m0 <SoftStar2004@gmail.com>
2021-09-25 17:15:47 +0300

    ваіва

---
+++ Migrations/20210918054734_mdidb1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pmdi.Migrations
+{
+    public partial class mdidb1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DiagnosisPatientsId",
+                table: "DiagnosisPatientsDoc",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiagnosisPatientsDoc_DiagnosisPatientsId",
+                table: "DiagnosisPatientsDoc",
+                column: "DiagnosisPatientsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DiagnosisPatientsDoc_DiagnosisPatients_DiagnosisPatientsId",
+                table: "DiagnosisPatientsDoc",
+                column: "DiagnosisPatientsId",
+                principalTable: "DiagnosisPatients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DiagnosisPatientsDoc_DiagnosisPatients_DiagnosisPatientsId",
+                table: "DiagnosisPatientsDoc");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DiagnosisPatientsDoc_DiagnosisPatientsId",
+                table: "DiagnosisPatientsDoc");
+
+            migrationBuilder.DropColumn(
+                name: "DiagnosisPatientsId",
+                table: "DiagnosisPatientsDoc");
+        }
+    }
+}
---
+++ Migrations/20210918070741_mdidb4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pmdi.Migrations
+{
+    public partial class mdidb4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TokenSharedViewPatient",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    UserId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Tsi = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    PatientId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    DateExpire = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DeleteAfterView = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TokenSharedViewPatient", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TokenSharedViewPatient_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TokenSharedViewPatient_PatientId",
+                table: "TokenSharedViewPatient",
+                column: "PatientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TokenSharedViewPatient");
+        }
+    }
+}
